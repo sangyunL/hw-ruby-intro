2f3105ccf0a2ee9c8657fee542015b8ab63b44f5
sangyun <socio1425bsh@daum.net>
2020-11-18 00:10:28 +0000

    my code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,97 @@
 # When done, submit this entire file to the autograder.
 
-# Part 1
+# part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.length>0
+    hap=0
+    arr.each { |value| hap+=value }
+    return hap
+  else
+    return 0
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  max1=0
+  max2=0
+  if arr.length>1
+    max1=arr.sort[-1]
+    max2=arr.sort[-2]
+    hap=max1+max2
+    return hap
+  elsif arr.length == 1
+    return arr[0]
+  else
+    return 0
+  end
 end
-
+  
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  result = false
+  if arr.length>1
+    for i in 1..arr.length-1
+      for j in 0..i-1
+        if arr[j]+arr[i]==n
+           result=true
+           break
+        end
+      end
+    end
+  end
+  return result
 end
 
 # Part 2
-
 def hello(name)
-  # YOUR CODE HERE
+	return "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s) 
+  if s =~ /\A(?=[^aeiou])(?=[a-z])/i
+    return true
+  else
+    return false  
+  end
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
-end
 
+def binary_multiple_of_4?(s)
+  if s=='0'
+    return true
+  end
+  
+  if s.length<3
+    return false
+  end
+  
+  s.each_char do |x|
+    unless x=='0' or x=='1'
+      return false
+    end
+  end
+  if s[-1]=='0' and s[-2]=='0' and s.to_i>=100
+    return true
+  end
+  false
+end
+ 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    @isbn=isbn
+    @price=price
+    if @isbn.empty? || @price<=0
+      raise ArgumentError
+    end
+  end
+  
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def price_as_string
+    "$#{'%.2f'%@price}"
+  end
+  
 end
